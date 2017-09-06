24379321dec10e9a95148a9b38b75ca837307082
cindy <cindy.nicolay@laposte.net>
2017-09-06 16:46:41 +0200

    input password and button submit html

---
+++ formulaire.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Serveur central de la NASA</title>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+
+      <form class="form-inline">
+        <div class="form-group">
+          <label for="inputPassword4">Password</label>
+          <input type="password" name="mot_de_passe" id="inputPassword4" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
+          <small id="passwordHelpInline" class="text-muted">
+            you never don't found!
+          </small>
+          <button type="submit" value="valider">Submit</button>
+        </div>
+      </form>
+
+    </body>
+</html>
